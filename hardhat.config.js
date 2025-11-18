478f5ef3c9fe2b8840f96fedf340d2a707bea801
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2025-11-18 19:28:01 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
+module.exports = {
+  solidity: "0.8.28",
+  network: {
+    ropsten:{
+      url:,
+      accounts: [`${}`],
+    }
+  }
+  
+};
---
+++ script/deploy.js
@@ -0,0 +1,18 @@
+const { ethers } = require("ethers");
+
+
+async function main () {
+
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
+
+    console.log("Token address", token.address);
+
+}
+
+main ()
+.then(()=> process.exit(0))
+.catch((error)=> {
+   console.error(error);
+   process.exit(1); 
+})
