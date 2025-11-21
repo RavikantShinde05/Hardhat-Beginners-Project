aff092e563ceba5bea0b8284c9c589e5ff875f6b
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2025-11-21 12:09:24 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-ethers");
+
+// this Private_key is confidential it must be key to safe place/file like ".env" should not be used
+// like this directly. This is just for understanding purpose.
+
+// const ALCHEMY_API = "";
+// const PRIVATE_KEY = "";
+
 module.exports = {
   solidity: "0.8.28",
   network: {
